9f31c492f2ffba048bce3bdeae775acc741687d7
Hugo L. Ruffo <hlruffo@gmail.com>
2024-05-18 11:08:48 -0300

    inserção de imagens no perfil de carros

--- cars/models.py
+++ cars/models.py
@@ -14,7 +14,9 @@ class Car(models.Model):
     brand = models.ForeignKey(Brand, related_name='car_brand', on_delete=models.PROTECT)
     factory_year = models.IntegerField(blank=True, null=True)
     model_year = models.IntegerField(blank=True, null=True)
+    plate = models.CharField(max_length=10, blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    photo = models.ImageField(upload_to='cars/', blank=True, null=True)
     
     def __str__(self):
         return self.model
