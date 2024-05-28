109865a4bc4c8fab477322d2006c81f2c4586261
Hugo L. Ruffo <hlruffo@gmail.com>
2024-05-28 20:39:48 -0300

    refac: reescreve cars_view como Classe

--- cars/urls.py
+++ cars/urls.py
@@ -2,9 +2,10 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
-from .views import cars_view, new_cars_view
+from .views import  new_cars_view, CarView#, cars_view,
 
 urlpatterns = [    
-    path('', cars_view, name='cars_list'),
+    #path('', cars_view, name='cars_list'),
+    path('', CarView.as_view(), name='cars_list'), # a chamada de classes depende da função as_view()
     path('new_car', new_cars_view, name='new_car'),
 ]
--- cars/views.py
+++ cars/views.py
@@ -1,15 +1,38 @@
 from django.shortcuts import render, redirect
 from .models import Car
 from .forms import CarForm
+
+from django.views import View
 # Create your views here.
 
-def cars_view(request):
-    carros = Car.objects.all().order_by('brand')  
-    return render(
-        request,
-        'cars.html',
-        {'cars': carros}
-        )
+# def cars_view(request):
+#     carros = Car.objects.all().order_by('brand')  
+#     search = request.GET.get('search')
+    
+#     if search:
+#         carros = Car.objects.filter(brand__icontains=search)
+    
+#     return render(
+#         request,
+#         'cars.html',
+#         {'cars': carros}
+#         )
+#REECREVENDO A FUNÇÃO ACIMA COM A CLASSE ABAIXO
+
+class CarView(View):
+    def get(self, request):
+        carros = Car.objects.all().order_by('brand')  
+        search = request.GET.get('search')
+        
+        if search:
+            carros = Car.objects.filter(brand__icontains=search)
+        
+        return render(
+            request,
+            'cars.html',
+            {'cars': carros}
+            )
+
     
 def new_cars_view(request):
     if request.method == 'POST':
