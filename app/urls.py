937dbb1199336927f7e633a969578270adf07bbf
Hugo L. Ruffo <hlruffo@gmail.com>
2024-05-28 20:14:43 -0300

    feat: login, logout e cadastro de usuários

--- app/urls.py
+++ app/urls.py
@@ -21,5 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('cars.urls')),
+    path('cars/', include('cars.urls')),
+    path('', include('accounts.urls')),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
