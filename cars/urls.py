e0f2d01104ac82f90c2a97e2a7a7a68bad20dba3
Hugo L. Ruffo <hlruffo@gmail.com>
2024-06-02 17:56:22 -0300

    refac: Muda exibição para ListView e Criação para CreateView

--- cars/urls.py
+++ cars/urls.py
@@ -2,11 +2,13 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
-from .views import   CarView    #, cars_view,new_cars_view,
+from .views import CarListView, NewCarCreateView    #, cars_view,new_cars_view,CarView,NewCarView, 
 
 urlpatterns = [    
     #path('', cars_view, name='cars_list'),
-    path('', CarView.as_view(), name='cars_list'), # a chamada de classes depende da função as_view()
+    #path('', CarView.as_view(), name='cars_list'), # a chamada de classes depende da função as_view()
+    path('', CarListView.as_view(), name='cars_list'), # a chamada de classes depende da função as_view()
     #path('new_car', new_cars_view, name='new_car'),
-    path('', NewCarView.as_view(), name='cars_list'), # a chamada de classes depende da função as_view()
+    #path('new_car', NewCarView.as_view(), name='new_car'), # a chamada de classes depende da função as_view()
+    path ('new_car', NewCarCreateView.as_view(), name='new_car'),
 ]
--- cars/views.py
+++ cars/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Car
 from .forms import CarForm
-
+from django.views.generic import ListView, CreateView
 from django.views import View
 # Create your views here.
 
@@ -19,19 +19,32 @@ from django.views import View
 #         )
 #REECREVENDO A FUNÇÃO ACIMA COM A CLASSE ABAIXO
 
-class CarView(View):
-    def get(self, request):
-        carros = Car.objects.all().order_by('brand')  
-        search = request.GET.get('search')
+# class CarView(View):
+#     def get(self, request):
+#         carros = Car.objects.all().order_by('brand')  
+#         search = request.GET.get('search')
         
+#         if search:
+#             carros = Car.objects.filter(brand__icontains=search)
+        
+#         return render(
+#             request,
+#             'cars.html',
+#             {'cars': carros}
+#             )
+#Reescrevendo com o Classe ListView
+
+class CarListView(ListView):
+    model = Car
+    template_name = 'cars.html'
+    context_object_name = 'cars'
+    
+    def get_queryset(self):
+        cars = super().get_queryset().order_by('model')
+        search = self.request.GET.get('search')
         if search:
-            carros = Car.objects.filter(brand__icontains=search)
-        
-        return render(
-            request,
-            'cars.html',
-            {'cars': carros}
-            )
+            cars = cars.filter(model__icontains=search)
+        return cars
 
     
 # def new_cars_view(request):
@@ -44,14 +57,21 @@ class CarView(View):
 #         form = CarForm()
 #     return render(request, 'new_car.html', {'form': form})
             
-class NewCarView(View):
-    def get(self, request):
-        form = CarForm()
-        return render(request, 'new_car.html', {'form': form})
+# class NewCarView(View):
+#     def get(self, request):
+#         form = CarForm()
+#         return render(request, 'new_car.html', {'form': form})
     
-    def post(self, request):
-        form = CarForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('cars_list')    
-        return render(request, 'new_car.html', {'form': form})
+#     def post(self, request):
+#         form = CarForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('cars_list')    
+#         return render(request, 'new_car.html', {'form': form})
+# Reescrevendo com a classe CreateView
+    
+class NewCarCreateView(CreateView):
+    model = Car
+    form_class = CarForm
+    template_name = 'new_car.html'
+    success_url = '/cars/'
